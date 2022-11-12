26db1047934a40905a62ec9675fc118ae80cf8bb
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-11-12 14:58:30 +0600

    add lesson 12/11

---
+++ Lessons/lesson 12.11.22/basics.py
@@ -0,0 +1,34 @@
+x = 10
+
+print(x)
+
+x = "test"
+
+print(x)
+
+x, y = 2, 3
+print(x, y)
+
+# swap = x
+# x = y
+# y = swap
+# print(x, y)
+
+#test comment
+
+x, y = y, x
+print(x, y)
+
+c, a, t = "cat"
+print(c, a, t)
+
+
+x = x+1
+print(x)
+
+x += 1
+print(x)
+x -= 2
+print(x)
+x *= 10
+print(x)
