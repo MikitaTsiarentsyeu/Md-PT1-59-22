4df8ae9036eaa4b895fe182b04b451fd0aed7220
KirillBalakirev <112400895+KirillBalakirev@users.noreply.github.com>
2022-11-14 16:13:06 +0300

    Create task1.py

---
+++ Tasks/Balakirev/task1.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+
+def deposit(money, time, percent):
+    capital = input('Include monthly capitalization in the terms of the contract?(Yes or No)')
+    if capital.lower() == 'yes':
+        return money * (1 + (percent/100)/365) ** (time*365)
+    else:
+        return ((money * percent * time) / 100) + money
+
+
+money = int(input('Enter the initial deposit amount in rubles'))
+time = int(input('Enter term of deposit in years'))
+percent = int(input('Enter the annual interest on the deposit'))
+final_amount = Decimal(str(deposit(money, time, percent)))
+print(final_amount.quantize(Decimal('1.00')))
