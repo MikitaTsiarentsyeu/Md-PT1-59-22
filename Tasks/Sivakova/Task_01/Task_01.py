533905da5d9606cc2dd525b151478defc874a2a9
Olga Sivakova <117565749+OlgaSivakova@users.noreply.github.com>
2022-11-15 10:57:54 +0300

    add new task

--- Tasks/Sivakova/Task_01/Task_01.py
+++ Tasks/Sivakova/Task_01/Task_01.py
@@ -1,9 +1,10 @@
 import time
+import decimal
 
 print("Welcome on our webpage!")
-sum=int(input("Please, enter your basic sum in BYN: "))
-per=int(input("For how long do you want to invest in years? "))
-am=int(input("Choose your percentage: "))
+sum=decimal.Decimal(input("Please, enter your basic sum in BYN: "))
+per=decimal.Decimal(input("For how long do you want to invest in years? "))
+am=decimal.Decimal(input("Choose your percentage: "))
 
 print("We are processing your request...")
 time.sleep(2)
@@ -17,13 +18,13 @@ m="month"
 y="year"
 n="none"
 
-income=int(sum*per*am)/(100)
-dep=int(income+sum)
+income=decimal.Decimal(sum*per*am)/(100)
+dep=decimal.Decimal(income+sum).__round__(2)
 
-capd=int(sum*((1+am/100/365))**per) 
-capw=int(sum*((1+am/100/4))**per)
-capm=int(sum*((1+am/100/12))**per)  
-capy=int(sum*((1+am/100/1))**per)
+capd=decimal.Decimal(sum*((1+am/100/365))**per).__round__(2) 
+capw=decimal.Decimal(sum*((1+am/100/4))**per).__round__(2)
+capm=decimal.Decimal(sum*((1+am/100/12))**per).__round__(2)  
+capy=decimal.Decimal(sum*((1+am/100/1))**per).__round__(2)
 
 
 
