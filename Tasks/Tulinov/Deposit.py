a896c1c881995fea7920dfd77573dbc2acd5c288
sssttas <se_tulinov@mail.ru>
2022-11-17 22:02:12 +0300

    add fill

---
+++ Tasks/Tulinov/Deposit.py
@@ -0,0 +1,16 @@
+import decimal
+
+a = decimal.Decimal(input("Введите начальную сумму вклада "))
+b = decimal.Decimal(input("Введите срок размещения в годах? "))
+c = decimal.Decimal(input("Годовая процентная ставка? "))
+d = decimal.Decimal(input("Если вам необходима капитализация поставьте 0, если ненужна поставьте 1? "))
+f = 12
+
+if d == 0:
+    S = (a * ((1 + ((c/100)/f))**(f*b)))
+    Y = decimal.Decimal(S)
+    print (S)
+
+if d == 1:
+    S = (a + a * (c/100))
+    print (S)
