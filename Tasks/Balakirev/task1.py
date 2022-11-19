b113fce02289f55888dc20455e38fc06e0d3fcba
KirillBalakirev <112400895+KirillBalakirev@users.noreply.github.com>
2022-11-19 08:58:09 +0300

    Update task1.py

--- Tasks/Balakirev/task1.py
+++ Tasks/Balakirev/task1.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 def deposit(money, time, percent):
     capital = input('Include monthly capitalization in the terms of the contract?(Yes or No)')
     if capital.lower() == 'yes':
-        return money * (1 + (percent/100)/365) ** (time*365)
+        return money * (1 + (percent/100)/12) ** (time*12)
     else:
         return ((money * percent * time) / 100) + money
 
