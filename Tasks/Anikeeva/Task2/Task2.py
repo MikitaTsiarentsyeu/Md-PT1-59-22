4e4de4d44f10aaf3ba4e566a385a2e3685f7d75f
OksanaAnikeeva <oks.anikeeva@gmail.com>
2022-11-24 22:18:53 +0300

    Update Task2.py
    add if user doesn't enter b

--- Tasks/Anikeeva/Task2/Task2.py
+++ Tasks/Anikeeva/Task2/Task2.py
@@ -5,6 +5,10 @@ effect1 = effect.replace ('+', '').replace ('y', '').replace ('=', '')
 result=effect1.split('x')
 h = decimal.Decimal(input("Please enter x:"))
 k=decimal.Decimal(result[0])
-b=decimal.Decimal(result[1])
-y=decimal.Decimal(k*h+b)
-print(f"The answer is:\n" ,y)
+b=decimal.Decimal()
+y = decimal.Decimal(k * h + b)
+if b == '':
+   b = 0.0
+else:
+   b=(result[1])
+print(f"The answer is:\n" , y)
