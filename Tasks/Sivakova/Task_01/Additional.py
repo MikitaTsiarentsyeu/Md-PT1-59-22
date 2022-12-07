877ed9a9857a09da24f7b4cc238753cd71f6051f
Olga Sivakova <117565749+OlgaSivakova@users.noreply.github.com>
2022-12-07 17:21:41 +0300

    add

---
+++ Tasks/Sivakova/Task_01/Additional.py
@@ -0,0 +1,5 @@
+d = {"one":1, "two":2, "three":3, "four":4, "five":5, "eight":8, "ten":10, "eleven":11, "thirteen":13, "seventeen":17, "nineteen":19}
+i = input().split()
+l = list(set([d[i] for i in i]))
+print(sum([i for i in l if i %2 != 0]))
+[print(l[i]*l[i+1]) if i%2==0 else print(l[i]+l[i+1]) for i in range(len(l)-1)]
