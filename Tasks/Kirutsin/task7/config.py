53d102f1b751ec3122070fa6a656aa1e5f1ca409
KONSTANTIN KIRUTIN <unitydeveloperkid@gmail.com>
2023-01-17 20:07:39 +0300

    task 7.00 reliz

---
+++ Tasks/Kirutsin/task7/config.py
@@ -0,0 +1,21 @@
+# config db
+path_bd = 'secure_bd'
+key = b'B2sb3twsFKJd36sj'
+
+# user table
+user_table = "personal_data"
+user_column_login = "login"
+user_column_password = "password"
+
+# user_cart
+user_cart_table = 'user_cart'
+user_cart_column_user = 'user'
+user_cart_column_product = 'product'
+user_cart_column_price = 'price'
+user_cart_column_currency = 'currency'
+
+# shop
+shop_table = 'shop'
+shop_column_product = 'product'
+shop_column_price = 'price'
+shop_column_currency = 'currency'
