bdaeaa6da5cf41a1e78199ac46ffc13e8012e7cf
Olga Sivakova <117565749+OlgaSivakova@users.noreply.github.com>
2023-02-06 17:20:28 +0300

    Delete Tasks/Sivakova/Task_01/webproject directory

--- Tasks/Sivakova/Task_01/webproject/programm/web/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Author, Post, DT
-
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(DT)
